1bf208aff10a7529a4b5ff99bc55498589d545a8
NguyenThienAn <nguyenthienan@mobcec.com>
2022-09-23 15:08:43 +0700

    router

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require("express");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", (req, res, next) => {
   console.log("this always run");
   next();
 });
 
-app.use("/product", (req, res, next) => {
-  res.send("<h2>This is product page</h2>");
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
-app.use("/", (req, res, next) => {
-  res.send("<h2>nguyenthienan</h2>");
+app.use((req, res, next) => {
+  res.status(404).send("<h2>404 Error</h2>");
 });
 
 app.listen(3000);
